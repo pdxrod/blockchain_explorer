562a7543ab1736718c8f423a8e8046fc875b5989
z <x@y.com>
2018-05-24 13:23:03 +0700

    Transaction tests pass

--- lib/blockchain_explorer/blockchain/transaction.ex
+++ lib/blockchain_explorer/blockchain/transaction.ex
@@ -1,39 +1,70 @@
 defmodule BlockChainExplorer.Transaction do
   alias BlockChainExplorer.Blockchain
-  alias BlockChainExplorer.Utils
 
-  defstruct vout: [], confirmations: 0, block: 0, relay_time: 0,
-    inputs: [], outputs: [], fee: 0.0, size: 0
+  @transaction %{
+    "vsize": 184,
+    "vout": [
+      %{
+        "value": 0.78152,
+        "scriptPubKey": %{
+          "type": "scripthash",
+          "reqSigs": 1,
+          "hex": "a9141a30c2cb52f5bd4fcbd8697b37b636f9b73ebedf87",
+          "asm": "OP_HASH160 1a30c2cb52f5bd4fcbd8697b37b636f9b73ebedf OP_EQUAL",
+          "addresses": [
+            "2MudhyEqJ7RzedMPXrReNXDcJ9Hch1AUqdv"
+          ]
+        },
+        "n": 0
+      },
+      %{
+        "value": 0.0,
+        "scriptPubKey": %{
+          "type": "nulldata",
+          "hex": "6a24aa21a9ed4967c81d1c2ae8c9a438982483213e5ada29ee685780a397a8cd22ce9e262255",
+          "asm": "OP_RETURN aa21a9ed4967c81d1c2ae8c9a438982483213e5ada29ee685780a397a8cd22ce9e262255"
+        },
+        "n": 1
+      }
+    ],
+    "vin": [
+      %{
+        "sequence": 4294967295,
+        "coinbase": "03190b14042126065b726567696f6e312f50726f6a65637420425443506f6f6c2f020df278d618000000000000"
+      }
+    ],
+    "version": 2,
+    "txid": "53a9f958519b4cbecb34c1315980e833a67b6aa5c9e242aab74f49d0a5e9bfb6",
+    "size": 211,
+    "locktime": 0,
+    "hash": "98e8e6534e0fb4a515f314ea7bd7f8d4b63803894feca243c8c9608e0aa8e679"
+  }
+
+  defstruct vsize: 0, outputs: [], inputs: [], version: 0, txid: "", size: 0, hash: ""
 
   def get_transactions( block ) do
     block[ "tx" ]
   end
 
-  def total_value( vin_or_vout ) do
-    Enum.reduce vin_or_vout, 0.0, fn( tuple, acc ) ->
+  def total_value( outputs ) do
+    Enum.reduce outputs, 0.0, fn( tuple, acc ) ->
       tuple[ "value" ] + acc
     end
   end
 
-  def decode_transaction( block, tuple ) do
+  def decode_transaction( tuple ) do
     case elem( tuple, 0 ) do
       :ok ->
         transaction = elem( tuple, 1 )
         vout = transaction[ "vout" ]
-IO.puts "\n\ntransaction:"
-IO.puts Poison.encode! elem( tuple, 1 ), pretty: true
-IO.puts "\n\n"
-IO.puts Utils.typeof transaction
-IO.puts "\n\n"
-IO.puts "total value"
-IO.puts total_value( vout )
-IO.puts "\n\n"
       _ ->
-        transaction = { elem( tuple, 0 ) }
+        transaction = %{ error: tuple }
     end
 
-    %BlockChainExplorer.Transaction{ block: block[ "height" ], vout: vout }
-
+    %BlockChainExplorer.Transaction{ outputs: transaction[ "vout" ],
+      inputs: transaction[ "vin" ], version: transaction[ "version" ],
+      txid: transaction[ "txid" ], size: transaction[ "size" ],
+      hash: transaction[ "hash" ], vsize: transaction[ "vsize" ] }
   end
 
   defp get_hex( transaction ) do
--- test/blockchain_explorer/blockchain/transaction_test.exs
+++ test/blockchain_explorer/blockchain/transaction_test.exs
@@ -19,19 +19,15 @@ defmodule BlockChainExplorer.TransactionTest do
     test "decode a transaction" do
       result = Blockchain.get_latest_block()
       block = elem( result, 1 )
-      hash = block[ "previousblockhash" ]
-      result = Blockchain.getblock( hash )
-      [ hd | tl ] = Transaction.get_transactions( block )
+      [ hd | _ ] = Transaction.get_transactions( block )
       tuple = Transaction.get_transaction( hd )
-      decoded = Transaction.decode_transaction( block, tuple )
-      assert decoded.block == block[ "height" ]
-      assert Transaction.total_value( decoded.vout ) > 0.0
-      assert decoded.confirmations > 0
-      assert decoded.relay_time
-      assert length( decoded.inputs ) > 0
-      assert length( decoded.outputs ) > 0
-      assert decoded.fee > 0.0
+      decoded = Transaction.decode_transaction( tuple )
+      assert Transaction.total_value( decoded.outputs ) > 0.0
+      assert List.first( decoded.inputs )[ "sequence" ] > 0
+      assert decoded.version > 0
+      assert decoded.txid =~ ~r/[0-9a-f]+/
       assert decoded.size > 0
+      assert decoded.hash =~ ~r/[0-9a-f]+/
     end
 
   end
