85d7b4605bad7bfaeeb30bdb5bae9be7b436e23b
z <x@y.com>
2018-06-02 19:59:36 +0200

    Not showing inputs and outputs yet

--- lib/blockchain_explorer_web/views/transaction_view.ex
+++ lib/blockchain_explorer_web/views/transaction_view.ex
@@ -5,7 +5,9 @@ defmodule BlockChainExplorerWeb.TransactionView do
   def mark_up_transaction( transaction_str ) do
     result = Transaction.get_transaction_tuple( transaction_str )
     case elem( result, 0 ) do
-      :ok    -> Transaction.decode_transaction( result )
+      :ok    -> t = Transaction.decode_transaction( result )
+      IO.inspect t
+      t
       :error -> Poison.encode!( String.downcase( elem( result, 1 )[ "message" ] ), pretty: true )
       _      -> Poison.encode!( "unknown error", pretty: true )
     end
