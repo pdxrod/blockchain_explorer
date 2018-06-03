c48c30d8acbbff301315a0574297fe85c06aadbc
z <x@y.com>
2018-06-03 19:04:17 +0200

    More progress showing inputs, outputs and addresses

--- lib/blockchain_explorer_web/views/transaction_view.ex
+++ lib/blockchain_explorer_web/views/transaction_view.ex
@@ -1,24 +1,45 @@
 defmodule BlockChainExplorerWeb.TransactionView do
   use BlockChainExplorerWeb, :view
 
+  def mark_up_outputs( outputs_list ) do
+
+IO.write "mark_up_outputs outputs_list - "
+IO.inspect outputs_list
+
+    case outputs_list do
+      [ head | tail ] -> mark_output( head ) <> mark_up_outputs( tail )
+      _ -> ""
+    end
+  end
+
   def mark_up_inputs( inputs_list ) do
-    if length( inputs_list ) > 0 do
-      case inputs_list do
-        [ head | tail ] -> mark_input( head ) <> mark_up_inputs( tail ) 
-        _ -> []
-      end
-    else
-      ""
+    case inputs_list do
+      [ head | tail ] -> mark_input( head ) <> mark_up_inputs( tail )
+      _ -> ""
+    end
+  end
+
+  defp mark_output( output ) do
+    """
+        N: #{ output.n }<br />\n
+        Addresses: <br />\n
+    """ <> mark_up_addresses( output.scriptpubkey.addresses )
+  end
+
+  defp mark_up_addresses( addresses_list ) do
+    case addresses_list do
+      [ head | tail ] -> "#{ head }<br />\n" <> mark_up_addresses( tail )
+      _ -> ""
     end
   end
 
   defp mark_input( input ) do
     """
     Sequence: #{ input.sequence }   <br />
-    Txid:     #{ input.txid     }   <br />
+    Txid:     #{ input.txid     }   <br />\n
     Scriptsig:                      <br />
-    asm: #{ input.scriptsig["asm"] }<br />
-    hex: #{ input.scriptsig["hex"] }<br />
+    asm: #{ input.scriptsig["asm"] }<br />\n
+    hex: #{ input.scriptsig["hex"] }<br />\n
     """
   end
 end
