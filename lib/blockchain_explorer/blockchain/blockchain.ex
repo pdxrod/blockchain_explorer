6b7a6ff100dd657cb5e0e737457565a87058572b
z <x@y.com>
2018-06-29 17:50:11 +0200

    Changed 'direction' to previousblockhash or nextblockhash

--- lib/blockchain_explorer/blockchain/blockchain.ex
+++ lib/blockchain_explorer/blockchain/blockchain.ex
@@ -61,12 +61,12 @@ defmodule BlockChainExplorer.Blockchain do
       n <= 1 ->
         blocks
       true ->
+        if direction != "previousblockhash" && direction != "nextblockhash", do: raise "direction should be previousblockhash or nextblockhash, not #{ direction }"
         new_block = get_next_or_previous_block( block, direction )
         if map_size( new_block ) > 0 do
           blocks = case direction do
             "previousblockhash" -> Tuple.append( blocks, new_block )
             "nextblockhash" -> Tuple.insert_at( blocks, 0, new_block )
-            _ -> raise "direction should be previousblockhash or nextblockhash, not #{ direction }"
           end
           get_n_blocks( new_block, n - 1, direction, blocks )
         else
