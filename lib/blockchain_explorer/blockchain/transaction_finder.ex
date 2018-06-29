48b3041d6b1f26d9ca112298b6edb738a1fb99ba
z <x@y.com>
2018-06-29 17:16:03 +0200

    Test for transaction finder

--- lib/blockchain_explorer/blockchain/transaction_finder.ex
+++ lib/blockchain_explorer/blockchain/transaction_finder.ex
@@ -3,15 +3,13 @@ defmodule BlockChainExplorer.TransactionFinder do
     Agent.start_link(fn -> [] end, name: __MODULE__ )
   end
 
+  defp is_in_block?( block_json, address_str ) do
+  end
+
+  defp is_in_transaction?( transaction_json, address_str ) do
+  end
+
   def find_transactions( address_str ) do
-
-
-  end
-
-  def is_in_block?( block_json, address_str ) do
-  end
-
-  def is_in_transaction?( transaction_json, address_str ) do
   end
 
 end
---
+++ test/blockchain_explorer/blockchain/transaction_finder_test.exs
@@ -0,0 +1,12 @@
+defmodule BlockChainExplorer.TransactionFinderTest do
+  use BlockChainExplorerWeb.ConnCase
+  alias BlockChainExplorer.Blockchain
+  alias BlockChainExplorer.TransactionFinder
+
+  describe "transaction finder test" do
+
+    test "find" do
+    end
+
+  end
+end
