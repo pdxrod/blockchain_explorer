8fab400bf2dda890d01bb075eacac0806d24eb9c
z <x@y.com>
2018-07-11 17:01:59 +0200

    Tidy up

--- assets/js/submit-form.js
+++ assets/js/submit-form.js
@@ -1,4 +1,5 @@
 
-  $( '#submit' ).on( 'click', function() {
+  $( "#submit" ).on( "click", function() {
+    // JQuery $() function doesn't work here
     document.getElementById( "please_wait_message" ).style.visibility = "visible";
   });
