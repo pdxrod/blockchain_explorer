dc471a724e6dabe2653bcf3b314f6a90878678d7
z <x@y.com>
2018-07-13 10:45:16 +0200

    Submit button won't disable

--- assets/js/submit-form.js
+++ assets/js/submit-form.js
@@ -1,4 +1,7 @@
 
-  $( "#blocks_show_submit_button" ).on( "click", function() {
-    document.getElementById( "please_wait_message" ).style.visibility = "visible";
-  });
+$( "#blocks_show_submit_button" ).on( "click", function() {
+  document.getElementById( "please_wait_message" ).style.visibility = "visible";
+});
+$( "#blocks_show_submit_button" ).addEventListener( "click", disable_blocks_submit_button(), false );
+function disable_blocks_submit_button() {
+};
