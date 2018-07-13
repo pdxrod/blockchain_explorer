d12ffdd0d147eb2d0b251bcc4855e075645b1bc7
z <x@y.com>
2018-07-13 11:13:38 +0200

    Stackoverflow.com to the rescue once again https://stackoverflow.com/questions/5691054/disable-submit-button-on-form-submit

--- assets/js/submit-form.js
+++ assets/js/submit-form.js
@@ -1,7 +1,8 @@
 
+$( "form#number" ).submit( function() {
+    $( this ).find( ":input[type=submit]" ).prop( "disabled", true );
+});
+// Thanks https://stackoverflow.com/questions/5691054/disable-submit-button-on-form-submit
 $( "#blocks_show_submit_button" ).on( "click", function() {
   document.getElementById( "please_wait_message" ).style.visibility = "visible";
 });
-$( "#blocks_show_submit_button" ).addEventListener( "click", disable_blocks_submit_button(), false );
-function disable_blocks_submit_button() {
-};
