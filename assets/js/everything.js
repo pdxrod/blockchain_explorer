cf75022bb9b20bcc3a29e740af1a7db190ce53ed
z <x@y.com>
2018-07-31 10:27:29 +0200

    Put all my Javascript in everything.js

--- assets/js/everything.js
+++ assets/js/everything.js
@@ -1,4 +1,12 @@
-console.log( "transactions_ajax" );
+console.log( "everything.js" );
+
+$( "form#number" ).submit( function() {
+    $( this ).find( ":input[type=submit]" ).prop( "disabled", true );
+});
+// Thanks https://stackoverflow.com/questions/5691054/disable-submit-button-on-form-submit
+$( "#blocks_show_submit_button" ).on( "click", function() {
+  document.getElementById( "please_wait_message" ).style.visibility = "visible";
+});
 
 $( document ).ready( function() {
   document.getElementById( "please_wait_message" ).style.visibility = "visible";
