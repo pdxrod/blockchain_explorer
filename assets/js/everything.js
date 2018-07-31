04f7e0c558130e1c184ce2236d3ebc02f741ad30
z <x@y.com>
2018-07-31 20:48:51 +0200

    Progress with Ajax

--- assets/js/everything.js
+++ assets/js/everything.js
@@ -11,27 +11,26 @@ if( form_number ) {
   });
 }
 
-function findTransactionsInBackground() {
-  var xhttp = new XMLHttpRequest();
-  console.log( "findTransactionsInBackground() " );
-  xhttp.onreadystatechange = function() {
-    if( this.readyState == 4 && this.status == 200 ) {
-      console.log( "findTransactionsInBackground " + this.responseText );
-      document.getElementById( "more_transactions" ).innerHTML = this.responseText;
-    }
-  };
-  var address = document.getElementById( "transactions_address" ).value;
-  console.log( "findTransactionsInBackground " + address );
-
-  xhttp.open( "GET", "/find/" + address, true );
-  xhttp.send();
-}
-
 var transactions_please_wait_message = document.getElementById( "transactions_please_wait_message" );
 if( transactions_please_wait_message ) {
   $( document ).ready( function() {
-    document.getElementById( "transactions_please_wait_message" ).style.visibility = "visible";
-    console.log( "Calling findTransactionsInBackground() " );
-    findTransactionsInBackground();
+    transactions_please_wait_message.style.visibility = "visible";
+    console.log( "Calling ajax " );
+    var xhttp = new XMLHttpRequest();
+    console.log( "ajax() " );
+    xhttp.onreadystatechange = function() {
+      if( this.readyState == 4 && this.status == 200 ) {
+        console.log( "ajax " + this.responseText );
+        document.getElementById( "more_transactions" ).innerHTML = this.responseText;
+      }
+    };
+
+    var transactions_address = document.getElementById( "transactions_address" );
+    console.log( "transactions_address " + transactions_address );
+    var address = transactions_address.innerHTML.trim();
+    console.log( "ajax " + address );
+
+    xhttp.open( "GET", "/find/" + address, true );
+    xhttp.send();
   });
 }
