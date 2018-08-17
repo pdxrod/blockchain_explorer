4af5520532b61fa07b0051eaef572a95fd4a5d5e
z <x@y.com>
2018-08-17 09:51:05 +0000

    Just before simplifying %Transaction

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -2,8 +2,8 @@ ExUnit.start()
 pair = System.cmd "ps", ["auxw"]
 strs = elem( pair, 0 )
 bitcoind_running = strs =~ ~r/bitcoind/
-if ! bitcoind_running do
-  raise "\n\nThe tests depend on bitcoind running - see start-bitcoind.sh\nto start bitcoind in test mode, and wait a couple of minutes\n"
-else
+if bitcoind_running do
   IO.puts "\n*** These tests are slow, because they make thousands of RPC requests to bitcoind ***\n"
+else
+  raise "\n\nThe tests depend on bitcoind running - see start-bitcoind.sh\nto start bitcoind in test mode, and wait a couple of minutes\n"
 end
