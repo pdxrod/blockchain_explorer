19f9375a66e049e4690df09129c7d3fdd8be2abe
z <x@y.com>
2018-11-13 14:10:19 -0800

    Test for sending money to addresses

--- test/blockchain_explorer/blockchain/address_test.exs
+++ test/blockchain_explorer/blockchain/address_test.exs
@@ -7,23 +7,41 @@ defmodule BlockChainExplorer.AddressTest do
 
   describe "addresses" do
 
+    defp address_list( outputs ) do
+      case outputs do
+        [] -> []
+        _ ->
+          [hd | tl] = outputs
+          if hd && hd.scriptpubkey && hd.scriptpubkey.addresses && length( hd.scriptpubkey.addresses ) > 0 do
+            [Enum.at( hd.scriptpubkey.addresses, 0 )] ++ address_list( tl )
+          else
+            address_list( tl )
+          end
+      end
+    end
+
+    defp block_list( blocks ) do
+      case blocks do
+        [] -> []
+        _ ->
+          [head | tail] = blocks
+          block = Block.decode_block head
+          assert length( block.tx ) > 0
+          tx_hash = Enum.at( block.tx, 0 )
+          assert tx_hash =~ Utils.env :base_16_hash_regex
+          tx_tuple = Transaction.get_transaction_tuple tx_hash
+          decoded = Transaction.decode_transaction_tuple tx_tuple
+          address_list( decoded.outputs ) ++ block_list( tail )
+      end
+    end
+
     test "sending coins" do
       block = Blockchain.get_best_block()
       blocks = Blockchain.get_n_blocks( block, 100 )
-      for num <- 0..99 do
-        tuple = Enum.at( blocks, num )
-        block = Block.decode_block tuple
-        assert length( block.tx ) > 0
-        tx_hash = Enum.at( block.tx, 0 )
-        assert tx_hash =~ Utils.env :base_16_hash_regex
-        tx_tuple = Transaction.get_transaction_tuple tx_hash
-        decoded = Transaction.decode_transaction_tuple tx_tuple
-        for output <- decoded.outputs do
-          if output && output.scriptpubkey && output.scriptpubkey.addresses && length( output.scriptpubkey.addresses ) > 0 do
-            address = Enum.at( output.scriptpubkey.addresses, 0 )
-            Blockchain.sendtoaddress(address, :rand.uniform( 6 ) + 1)
-          end
-        end
+      uniq = Enum.uniq( block_list( blocks ))
+      assert length( uniq ) > 0
+      for address <- uniq do
+        Blockchain.sendtoaddress(address, :rand.uniform( 6 ) + 1)
       end
     end
 
