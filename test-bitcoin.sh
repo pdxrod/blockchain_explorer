27a26c62969172c38f4b9807046cf18cb77b667c
z <x@y.com>
2018-11-14 16:15:07 -0800

    Got test-bitcoin.sh working with regtest mode

--- test-bitcoin.sh
+++ test-bitcoin.sh
@@ -1,5 +1,6 @@
-if [[ "" == "$2" ]] ; then
-    echo "Takes two arguments - rpc user name and rpc password - these should be set in $HOME/.bitcoin/bitcoin1.conf"
+if [[ "" == "$3" ]] ; then
+    echo "Takes three arguments. rpc user name and rpc password should be set in $HOME/.bitcoin/bitcoin1.conf"
+    echo "The third argument is 'regtest', 'testnet' or 'mainnet' - see README.md "
     exit 1
 fi
 
@@ -14,10 +15,9 @@ echo ""
 curl --data-binary '{"jsonrpc":"1.0","method":"getblockchaininfo","params":[]}' http://$1:$2@localhost:18333
 echo ""
 echo "Getting new address and private key"
-echo bitcoin-cli -testnet -conf=$HOME/.bitcoin/bitcoin1.conf getnewaddress
-address=`bitcoin-cli -testnet -conf=$HOME/.bitcoin/bitcoin1.conf getnewaddress`
+echo bitcoin-cli -$3 -conf=$HOME/.bitcoin/bitcoin1.conf getnewaddress
+address=`bitcoin-cli -$3 -conf=$HOME/.bitcoin/bitcoin1.conf getnewaddress`
 echo Address $address
-echo bitcoin-cli -testnet -conf=$HOME/.bitcoin/bitcoin1.conf dumpprivkey $address
-key=`bitcoin-cli -testnet -conf=$HOME/.bitcoin/bitcoin1.conf dumpprivkey $address`
+echo bitcoin-cli -$3 -conf=$HOME/.bitcoin/bitcoin1.conf dumpprivkey $address
+key=`bitcoin-cli -$3 -conf=$HOME/.bitcoin/bitcoin1.conf dumpprivkey $address`
 echo Private Key $key
-
