172c26a0212b686f03e6e9b6e3ec591c9a11c88a
z <x@y.com>
2018-12-23 13:14:32 -0800

    Made db_seeder refresh much less frequently

--- lib/blockchain_explorer/blockchain/db_seeder.ex
+++ lib/blockchain_explorer/blockchain/db_seeder.ex
@@ -10,10 +10,10 @@ defmodule BlockChainExplorer.DbSeeder do
   def init(state) do
     BlockChainExplorer.Transaction.seed_db_and_get_a_useful_transaction()
 
-    Process.sleep(10_000)
+    Process.sleep(100_000)
 
-    # Process will send :timeout to self after 1 second
-    {:ok, state, 1_000}
+    # Process will send :timeout to self after 100 seconds
+    {:ok, state, 100_000}
   end
 
   @impl true
