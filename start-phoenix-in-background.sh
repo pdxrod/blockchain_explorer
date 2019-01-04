f0605a89f234565e5702ae80a195474fc9c6ba63
z <x@y.com>
2019-01-03 17:39:58 -0800

    Changed start-phoenix script to prod, doing mix phx.digest to comppile javascript and css before startting

--- start-phoenix-in-background.sh
+++ start-phoenix-in-background.sh
@@ -1,6 +1,3 @@
-if [[ "" == "$1" ]] ; then
-   echo "Give your secret key base as a parameter to this script"
-   exit 1
-fi
+MIX_ENV=prod mix phx.digest
+MIX_ENV=prod PORT=4000 elixir --detached -S mix phx.server
 
-MIX_ENV=prod PORT=4000 SECRET_KEY_BASE=$1 elixir --detached -S mix phx.server
