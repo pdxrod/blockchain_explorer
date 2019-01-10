d0684350db31f0f612325dbdcafb14f977853913
z <x@y.com>
2019-01-10 07:34:10 -0800

    Changes needed to make blockchain explorer work in production

--- compile-javascript.sh
+++ compile-javascript.sh
@@ -3,6 +3,10 @@ if [[ ! -f assets/node_modules/brunch/bin/brunch ]] ; then
 	exit 1
 fi
 
+if [[ ! -d priv/static ]] ; then
+  mkdir -p priv/static
+fi
+
 if [[ -d priv/static/js ]] ; then
       rm priv/static/js/*
 fi
--- start-phoenix-in-background.sh
+++ start-phoenix-in-background.sh
@@ -1,3 +1,4 @@
+./compile-javascript.sh
 MIX_ENV=prod mix phx.digest
 MIX_ENV=prod PORT=4000 elixir --detached -S mix phx.server
 
