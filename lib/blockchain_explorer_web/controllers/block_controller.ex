e456ad6d10ce9467c5fb6367580575d7635f8b76
z <x@y.com>
2020-03-25 10:08:17 -0700

    Made it say 'No blocks' if Transaction.seed_db_and_get_a_useful_transaction returns an empty map

--- lib/blockchain_explorer_web/controllers/block_controller.ex
+++ lib/blockchain_explorer_web/controllers/block_controller.ex
@@ -143,6 +143,7 @@ defmodule BlockChainExplorerWeb.BlockController do
       _ ->
         a_transaction = Transaction.seed_db_and_get_a_useful_transaction()
         case a_transaction do
+          %{} -> show_error conn, "show.html", %{error: "No blocks"}
           %{error: _} -> show_error conn, "show.html", a_transaction
           _ ->
             addresses = Transaction.get_addresses a_transaction.id
