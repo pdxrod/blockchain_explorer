2ac111f78bac15418e459840c839640e4ea428c4
z <x@y.com>
2020-05-02 09:16:41 -0700

    Beginning of integrating bitcoin-elixir - test for Bitcoin.Script

---
+++ test/blockchain_explorer/blockchain/bitcoin_test.exs
@@ -0,0 +1,13 @@
+defmodule BlockChainExplorer.BitcoinTest do
+  use BlockChainExplorerWeb.ConnCase
+  
+  alias BlockChainExplorer.Utils
+  alias Bitcoin.Script
+
+  describe "bitcoin" do
+
+    test "script" do
+
+    end
+  end
+end
