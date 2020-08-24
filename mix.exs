e41640bf28e36d42fecf68d1d92aceaf23c84b6d
Rod McLaughlin <rodmclaughlin@gmail.com>
2020-08-24 06:28:57 +0100

    [integrate-bitcoin-elixir]: Upgraded to Elixir 1.10.4

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule BlockChainExplorer.Mixfile do
     [
       app: :blockchain_explorer,
       version: "0.0.1",
-      elixir: "~> 1.4",
+      elixir: "~> 1.10.4",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
